0bd2a795d089c8349686db278b832ffceece82a0
trzeciak20p <m.trzeciak.p20@gmail.com>
2026-01-14 13:48:20 +0100

    complex output multiplication signs

--- classes/complex.cpp
+++ classes/complex.cpp
@@ -14,16 +14,14 @@ const double Complex::arg(){
 }
 
 std::string Complex::getExponential(){
-    return std::to_string(abs()) + "e^(" + std::to_string(arg()) + "i)"; 
+    return std::to_string(abs()) + "*e^(" + std::to_string(arg()) + "*i)"; 
 } 
 std::string Complex::getPolar(){
     const std::string argument = std::to_string(arg());
-    return std::to_string(abs()) + "(cos(" + argument + ") + sin(" + argument + ")i)";
-    // const double absolute = abs();
-    // std::cout << absolute << "(" << real / absolute << ", " << imaginary / absolute <<")\r\n";
+    return std::to_string(abs()) + "*(cos(" + argument + ") + sin(" + argument + ")*i)";
 }
 std::string Complex::getRectangular(){
-    return  std::to_string(real) + " + (" + std::to_string(imaginary) + ")i"; 
+    return  std::to_string(real) + " + (" + std::to_string(imaginary) + ")*i"; 
 }
 
 

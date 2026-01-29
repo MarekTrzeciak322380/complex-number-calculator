3711798d976603638b700c09db9aacde84843191
trzeciak20p <m.trzeciak.p20@gmail.com>
2026-01-29 10:50:26 +0100

    deleted real

--- classes/real.cpp
+++
@@ -1,8 +0,0 @@
-#include "real.h"
-
-Real::Real(int number) : value{number}{};
-Real::Real(std::string str){};
-
-std::ostream &operator<<(std::ostream &out, const Real &r){
-    return out << r.value;
-}
